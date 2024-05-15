e66a1907dd1e4210d01a0f33f9d858f05bc54caf
Kevin Choi <kevinnwjchoi@gmail.com>
2024-05-14 20:04:22 -0400

    complete lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,3 +1,32 @@
+# #!/usr/bin/env python
+
+# from user import User
+
+# import random
+
+# class Teacher(User):
+
+#     def __init__(self, first_name, last_name):
+#         super().__init__(first_name, last_name)
+#         self._knowledge = [""]
+
+#     @property
+#     def knowledge(self):
+#         """This is the knowledge property"""
+#         return self._knowledge
+    
+#     @knowledge.setter
+#     def knowledge(self, knowledge):
+#         """"Check knowledge is a list, and greater than 0 length"""
+#         if isinstance(knowledge, list) and len(knowledge) > 0:
+#             self._knowledge = knowledge
+#         else: 
+#             print("Not long enough ")
+
+        
+#     def teach(self):
+#         pass
+
 #!/usr/bin/env python
 
 from user import User
@@ -6,5 +35,24 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [""]
+
+    @property
+    def knowledge(self):
+        """This is the knowledge property"""
+        return self._knowledge
+    
+    @knowledge.setter
+    def knowledge(self, knowledge):
+        """"Check knowledge is a list, and greater than 0 length"""
+        if isinstance(knowledge, list) and len(knowledge) > 0:
+            self._knowledge = knowledge
+        else: 
+            print("Not long enough ")
+
+        
     def teach(self):
-        pass
+        i = random.randint(0,len(self.knowledge))
+        return self.knowledge[i]
